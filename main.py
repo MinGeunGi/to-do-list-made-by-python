0ea8c25dd7a102446db4cf8251fdbaa6d683bda3
MinGeunGi <coolcool0518@naver.com>
2024-01-06 11:39:51 +0900

    todo 전체 조회, 일자별 조회

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ def run():  # 기능 선택
       print('해야 할 일 작성')
       set_todo(con, cur)
     if gineung_print == '2':
-      print('완료 표시')
+      print('전체 출력')
+      show_ToDo_list(cur, con)
       
     if gineung_print == '3':
-      print('특정 날짜 조회')
+      print('특정 날짜 todo 출력')
+      show_one_date_todo(cur)
       
     if gineung_print == '4':
       print('해야 할 일 삭제')
@@ -44,17 +46,42 @@ def set_todo(con, cur): # to do list 입력
   
     
   print(input_todo)
-  today_dates = datetime.today().strftime('%Y/%n/%d')
-  # INSERT INTO todo('created_date', 'contentes') VALUES
+  today_dates = datetime.today().strftime('%Y/%m/%d')
+  
   for todo in input_todo:
     cur.execute(
         f"INSERT INTO todo('created_date', 'contentes') VALUES('{today_dates}', '{todo}')"
     )
     con.commit()
     
-  
-  
     
+def show_ToDo_list(cur, con):
+    #모든 학생성적 조회
+    print("전체 출력")
+    
+    
+    result = cur.execute("SELECT seq, created_date, contentes, done, finish_date FROM todo")
+    todo = result.fetchall()
+    for data in todo:
+        print(data[0], data[1], data[2], data[3])
+      
+  
+def show_one_date_todo(cur):
+    
+    print("오늘 날짜 todo출력")
+    print('예시 : 2024/01/01')
+    
+    when = input()
+    if len(when) == 0:
+      when = datetime.today().strftime('%Y/%m/%d')
+      
+    result = cur.execute(f"SELECT seq, created_date, contentes, done, finish_date FROM todo WHERE created_date = '{when}'")
+    
+    todo = result.fetchall()
+    if len(todo) == 0:
+        print("오늘의 할일을 출력 합니다.")
+    for data in todo:
+        print(data[0], data[1], data[2], data[3])
 
 
 
